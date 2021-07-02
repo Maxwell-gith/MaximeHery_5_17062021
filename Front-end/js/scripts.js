96693f31b5ed0d5a14e412e00fc036d0f52a68d4
Maxime <herymaxime@gmail.com>
2021-07-02 16:35:02 +0200

    affichage_products

--- Front-end/js/scripts.js
+++ Front-end/js/scripts.js
@@ -6,19 +6,46 @@
 
 //REQUETE API
 fetch("http://localhost:3000/api/cameras")
-    .then(function(res){
-        if (res.ok){
-            return res.json();
+    .then( data => data.json())
+    .then( jsonListProducts => {
+        for(let jsonProducts of jsonListProducts){
+            let product = new Products(jsonProducts);
+            document.getElementById("container").innerHTML += `<div class="col mb-5">
+                                                                    <div class="card h-100">
+                                                                        <img class="card-img-top" src=${jsonProducts.imageUrl} alt="..." />
+                                                                        <div class="card-body p-4">
+                                                                            <div class="text-center">
+                                                                                <h5 class="fw-bolder">${jsonProducts.name}</h5>
+                                                                                <div class="d-flex justify-content-center small text-warning mb-2">
+                                                                                    <div class="bi-star-fill"></div>
+                                                                                    <div class="bi-star-fill"></div>
+                                                                                    <div class="bi-star-fill"></div>
+                                                                                    <div class="bi-star-fill"></div>
+                                                                                    <div class="bi-star-fill"></div>
+                                                                                </div>
+                                                                                <!-- Product price-->
+                                                                                <span>${jsonProducts.price/100}€</span>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                                                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="item_page.html">Voir fiche produit</a></div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>`
+
+
         }
     })
-    .then(function(value){
-        console.log(value);
-    })
-    .catch(function(err){
 
-    });
 
-// INTEGRATION HTML
+class Products{
+    constructor(jsonProducts){
+        jsonProducts && Object.assign(this, jsonProducts)
+    }
+}
 
-document.getElementById("container");
-
+class ProductsManager{
+    constructor(listProducts){
+        this.listProducts = listProducts;
+    }
+}
