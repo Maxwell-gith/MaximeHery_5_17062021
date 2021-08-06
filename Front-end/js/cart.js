082cf9917e43870ef4cf96b83e7a650edfd45351
Maxime <herymaxime@gmail.com>
2021-08-06 11:30:59 +0200

    form

--- Front-end/js/cart.js
+++ Front-end/js/cart.js
@@ -5,9 +5,11 @@ let cartContainer = document.getElementById('cartContainer');
 
 if(cart === null || cart < 1){
     cartContainer.innerHTML = 
-    `<div class="text-center mt-2 mb-5 table">
+    `<div class="text-center mt-2 mb-5">
         <h1 class="display-5 fw-bolder text-center">est vide...</h1>
-    </div>    `;
+        <a href="../index.html" class="btn btn-dark mt-5">Remplissez-le ! <i class="fas fa-camera-retro"></i></a>
+    </div>`;
+
 }else {
     let cartView = [];
     for(i = 0; i < cart.length; i++) {
@@ -24,12 +26,40 @@ if(cart === null || cart < 1){
         <tr id="totalPriceContainer">
 
         </tr>
-    </table>`;
+    </table>
+    <form>
+        <div>
+            <div class="form-group col-md-6 mt-3">
+                <input type="text" class="form-control" placeholder="Nom">
+            </div>
+            <div class="form-group col-md-6 mt-3">
+                <input type="text" class="form-control" placeholder="Prénom">
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6 mt-3">
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group col-md-6 mt-3">
+            <input type="text" class="form-control" id="inputAddress" placeholder="Adresse">
+        </div>
+        <div class="form-group col-md-6 mt-3">
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Complément d'adresse">
+        </div>
+        <div class="form-group col-md-6 mt-3">
+            <input type="text" class="form-control" id="inputCity" placeholder="Ville">
+        </div>
+        <div class="form-group col-md-6 mt-3">
+            <input type="number" class="form-control" id="inputpostalcode" placeholder="Code Postal">
+        </div>
+        <button type="submit" class="btn btn-dark mt-3">Commander</button>
+    </form>`;
     };
 
     if (i == cart.length) {
         cartContainer.innerHTML = cartView;
-    }
+    };
 };
 
 const itemTotalPrice = () => {
